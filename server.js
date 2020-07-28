f402d815928545d3a3e78bb70703731070d17c06
thaisviana <thais@hub9.co>
2020-07-28 08:08:59 -0300

    midlleaware

--- server.js
+++ server.js
@@ -2,7 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World da prof. Thais'))
+app.use(function (req, res, next) {
+    console.log('Middleaware')
+    next()
+})
+
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World da prof. Thais</h1>')
+})
+
 app.get('/outra-coisa', (req, res) => {
     let number = Math.random()
     number = Math.trunc(number * 10)
@@ -10,4 +19,4 @@ app.get('/outra-coisa', (req, res) => {
 })
 
 
-app.listen(port, () => console.log(`app listening at http://localhost:${port}`))
+app.listen(port, () => { console.log(`oi`) })
