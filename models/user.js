2440331663d0629863f372eaeebf2801c26b68ce
thaisviana <thais@hub9.co>
2020-08-04 08:03:58 -0300

    user api post, get, detail

---
+++ models/user.js
@@ -0,0 +1,10 @@
+const lista_usuarios = [
+    {id: 1 ,nome: "Carlos Eduardo", email: "carlos@bootcamp.com", senha : "a12345",is_active: true,  is_admin: false},
+    {id: 2 ,nome: "Eduardo", email: "eduardo@bootcamp.com", senha : "a12345",is_active: true,  is_admin: true},
+    {id: 3 ,nome: "Robson", email: "rob@bootcamp.com", senha : "a12345",is_active: false,  is_admin: false},
+    {id: 4 ,nome: "Thais", email: "ta@bootcamp.com", senha : "a12345",is_active: true,  is_admin: true},
+    {id: 5 ,nome: "Juliana", email: "ju@bootcamp.com", senha : "a12345",is_active: true,  is_admin: false}
+]
+
+
+module.exports = lista_usuarios;
--- routes/api/user.js
+++ routes/api/user.js
@@ -1,8 +1,56 @@
 const express = require('express');
+const lista_usuarios = require('../../models/user');
 const router = express.Router();
 
-router.get('/', (req, res, next)=> {
-  res.send({"user" : "a"})
+router.get('/:userId', (req, res, next)=> {
+  try{
+    let  usuario =  lista_usuarios.filter(u => u.id == req.params["userId"])
+    if (usuario.length > 0){
+      res.send(usuario[0])
+    }else{
+      res.status(404).send({"error" : "user not exist"})
+    }
+  }catch(err){
+    console.error(err.message)
+    res.status(500).send({"error" : "Server Error"})
+  }
 })
+
+router.get('/', (req, res, next)=> {
+  try{
+    res.send(lista_usuarios)
+  }catch(err){
+    console.error(err.message)
+    res.status(500).send({"error" : "Server Error"})
+  }
+})
+
+router.post('/',[], (req, res, next) => {
+  try{
+    const usuario = {id: 0}
+    const { nome, email, senha, is_active, is_admin } = req.body
+    usuario.nome = nome
+    usuario.email = email
+    usuario.senha = senha
+    usuario.is_active = is_active
+    usuario.is_admin = is_admin
+
+    if (!email){
+      res.status(400).send({"error" : "email required"})
+    }
+    else{
+      if(!(email.includes("@") && email.includes("."))){
+        res.status(400).send({"error" : "not valid email"})
+      }
+      usuario.id = lista_usuarios[lista_usuarios.length-1].id +1
+      lista_usuarios.push(usuario)
+      res.send(lista_usuarios)
+    }
+  }catch(err){
+    console.error(err.message)
+    res.status(500).send({"error" : "Server Error"})
+  }
+})
+
 
 module.exports = router;
