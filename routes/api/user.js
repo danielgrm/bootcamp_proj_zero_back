357784bb8072996c3b5a80289b6edd1d7f7263a7
thaisviana <thais@hub9.co>
2020-08-06 08:04:37 -0300

    put, patch, delete

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const lista_usuarios = require('../../models/user');
+let lista_usuarios = require('../../models/list');
+const Usuario = require('../../models/user');
 const router = express.Router();
+
 
 router.get('/:userId', (req, res, next)=> {
   try{
@@ -16,6 +18,75 @@ router.get('/:userId', (req, res, next)=> {
   }
 })
 
+router.delete('/:userId', (req, res, next)=> {
+  try{
+    let  usuario =  lista_usuarios.filter(u => u.id == req.params["userId"])
+    if (usuario.length > 0){
+      lista_usuarios = lista_usuarios.filter(u => u.id != req.params["userId"])
+      res.send(lista_usuarios)
+    }else{
+      res.status(404).send({"error" : "user not exist"})
+    }
+  }catch(err){
+    console.error(err.message)
+    res.status(500).send({"error" : "Server Error"})
+  }
+})
+
+router.put('/:userId', (req, res, next)=> {
+  try{
+    let  usuario =  lista_usuarios.filter(u => u.id == req.params["userId"])
+    if (usuario.length > 0){
+      usuario = usuario[0]
+      let { nome, email, senha, is_active, is_admin } = req.body
+      usuario.nome = nome
+      usuario.email = email
+      usuario.senha = senha
+      usuario.is_active = is_active
+      usuario.is_admin = is_admin
+
+      res.send(usuario)
+    }else{
+      res.status(404).send({"error" : "user not exist"})
+    }
+  }catch(err){
+    console.error(err.message)
+    res.status(500).send({"error" : "Server Error"})
+  }
+})
+
+router.patch('/:userId', (req, res, next)=> {
+  try{
+    let  usuario =  lista_usuarios.filter(u => u.id == req.params["userId"])
+    if (usuario.length > 0){
+      usuario = usuario[0]
+      let { nome, email, senha, is_active, is_admin } = req.body
+      if (nome){
+        usuario.nome = nome
+      }
+      if (email){
+        usuario.email = email
+      }
+      if (senha){
+        usuario.senha = senha
+      }
+      //TODO: validar undefined
+      if (is_active){
+        usuario.is_active = is_active
+      }
+      if (is_admin){
+        usuario.is_admin = is_admin
+      }
+      
+      res.send(usuario)
+    }else{
+      res.status(404).send({"error" : "user not exist"})
+    }
+  }catch(err){
+    console.error(err.message)
+    res.status(500).send({"error" : "Server Error"})
+  }
+})
 router.get('/', (req, res, next)=> {
   try{
     res.send(lista_usuarios)
@@ -27,13 +98,8 @@ router.get('/', (req, res, next)=> {
 
 router.post('/',[], (req, res, next) => {
   try{
-    let usuario = { id: 0 }
-    const { nome, email, senha, is_active, is_admin } = req.body
-    usuario.nome = nome
-    usuario.email = email
-    usuario.senha = senha
-    usuario.is_active = is_active
-    usuario.is_admin = is_admin
+    let { nome, email, senha, is_active, is_admin } = req.body
+    let usuario = new Usuario(id=0, nome=nome, email=email, senha=senha, is_active=is_active, is_admin=is_admin)
 
     if (!email){
       res.status(400).send({"error" : "email required"})
@@ -42,6 +108,7 @@ router.post('/',[], (req, res, next) => {
       if(!(email.includes("@") && email.includes("."))){
         res.status(400).send({"error" : "not valid email"})
       }
+      // TODO: tranformar no método de classe
       usuario.id = lista_usuarios[lista_usuarios.length-1].id +1
       lista_usuarios.push(usuario)
       res.send(lista_usuarios)
